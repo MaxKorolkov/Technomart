5266116320bc03c7c82cd88b7fb55195d1078de8
Maxim Korolkov <maximkorolkov1@gmail.com>
2016-04-02 02:30:35 +0300

    Ready to prodaction

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var popupButton = $(".popup-action"),
 		popup = $(".modal-content"),
 		popupClose = $(".modal-content-close"),//для обратной формы и карты - обработка закрытия одинаковая
 		mapButton = $(".map-action"),
-		map = $(".modal-map");
+		map = $(".modal-map"),
+		featuresButton = $(".features-button"),
+		featuresRight = $(".features-right");
 
 popupButton.click(function () {
 	event.preventDefault();
@@ -10,7 +12,7 @@ popupButton.click(function () {
 	popup.find("#input-name").focus();
 });
 
-popupClose.click(function () {
+popupClose.click(function () {//для обратной формы и карты - обработка закрытия одинаковая
 	event.preventDefault();
 	popup.css("display", "none");
 	map.css("display", "none");
@@ -21,5 +23,27 @@ mapButton.click(function () {
 	map.css("display", "block");
 });
 
+function numberActiveFeature () { //определение номера элемента массива при клике
+	for(var i = 0; i < featuresButton.length; i++) {
+		if($(featuresButton[i]).hasClass("features-button-active")) return i;		
+	}
+}
+
+featuresButton.click(function () { //переключение преимуществ
+	$(".features-button-active").removeClass("features-button-active");
+	$(this).addClass("features-button-active");
+	var numberFeature = numberActiveFeature();
+	$(".features-active").removeClass("features-active");
+	$(featuresRight[numberFeature]).addClass("features-active");
+});
+
+$(".big-category-item").click(function () { //слайдер
+	if($("#radio1").prop("checked")) {
+		$("#radio2").prop("checked", true);
+	}
+	else {
+		$("#radio1").prop("checked", true);
+	}
+});
 
 
